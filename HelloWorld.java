12ff6b91984ce79f59f978c351dc8cb49a7bbade
srnFeb26 <appolla.sreedhar@gmail.com>
2020-12-25 20:10:44 -0500

    Second Message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,9 @@
-
-
-
-
-
-
-
-
-
 public class HelloWorld{
 //Hello World program
 //To Enter this program type
 //vim HelloWorld.java
 //press Enter
+public static void main(String[] args){
+
+}
 }
